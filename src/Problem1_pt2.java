8e161b6661ce05968cc9e596e0263071f6fec792
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2023-12-06 23:52:13 -0300

    Part 2 day 1

--- src/Problem1_pt2.java
+++ src/Problem1_pt2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 
 public class Problem1_pt2 {
@@ -40,29 +41,50 @@ public class Problem1_pt2 {
             }
         }
         public int returnSum() {
-            if (Character.isDigit(number1) && Character.isDigit(number2)) {
-                return Integer.parseInt(String.valueOf(number1) + number2);
-            } else {
-                System.err.println("Invalid characters for sum calculation");
-                return 0; // Or handle the error in a way that makes sense for your application
-            }
+            return Integer.parseInt(String.valueOf(number1) + number2);
+        }
+        public void reset(){
+            this.number1 = 0;
+            this.number2 = 0;
         }
     }
+    private static String replaceSpelledOutNumbers(String line){
+        //didn't find solution where you didn't hardcode this :C
+        line = line.replace("oneight", "18");
+        line = line.replace("twone", "21");
+        line = line.replace("fiveight", "58");
+        line = line.replace("sevenine", "79");
+        line = line.replace("eightwo", "82");
+        line = line.replace("eighthree", "83");
+        line = line.replace("nineight", "98");
+        line = line.replace("one", "1");
+        line = line.replace("two", "2");
+        line = line.replace("three", "3");
+        line = line.replace("four", "4");
+        line = line.replace("five", "5");
+        line = line.replace("six", "6");
+        line = line.replace("seven", "7");
+        line = line.replace("eight", "8");
+        line = line.replace("nine", "9");
+        return line;
+    }
     public static void main(String[] args) throws IOException {
-        String absPath = System.getProperty("user.dir") + "\\src\\inputPROBLEM1.txt";
-        System.out.println(absPath);
+        String absPath = Paths.get(System.getProperty("user.dir"), "src", "inputPROBLEM1_PT2.txt").toString();
         BufferedReader reader = new BufferedReader(new FileReader(absPath));
         String line;
-        Holder h = null;
+        Holder h = new Holder();
         int sum = 0;
         while ((line = reader.readLine()) != null) {
-            h = new Holder();
+            System.out.println(line);
+            line = replaceSpelledOutNumbers(line);
+            System.out.println(line);
             for (char c : line.toCharArray()) {
                 h.addNumber(c);
             }
             sum += h.returnSum();
+            h.reset();
         }
         reader.close();
-        System.out.println(sum); //-> Prints 53921
+        System.out.println(sum); //-> Prints 54676
     }
 }
