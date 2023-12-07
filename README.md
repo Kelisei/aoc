721be450b638598fa93db4253e0e5a5616f7a63d
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2023-12-07 16:47:36 -0300

    Problem 2 part 1 Lil optimizations

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Problem 1:
 * [**Part 1**](https://github.com/Kelisei/aoc_2023/blob/master/src/Problem1.java)
 * [**Part 2**](https://github.com/Kelisei/aoc_2023/blob/master/src/Problem1_pt2.java)
+## Problem 2:
+* [**Part 1**](https://github.com/Kelisei/aoc_2023/blob/master/src/Problem2.java)
--- src/Problem2.java
+++ src/Problem2.java
@@ -6,58 +6,63 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Problem2 {
-    private static class Set{
+    private static class Set {
         private int blue;
         private int red;
         private int green;
-        private static Pattern pBlue = Pattern.compile("(\\d+) blue"); // (\d+) -> Captures any number
-        private static Pattern pRed = Pattern.compile("(\\d+) red");
-        private static Pattern pGreen = Pattern.compile("(\\d+) green");
-        public Set(){
-            blue=0;
-            red=0;
-            green=0;
+        private static final Pattern P_BLUE = Pattern.compile("(\\d+) blue"); // (\d+) -> Captures any number
+        private static final Pattern P_RED = Pattern.compile("(\\d+) red");
+        private static final Pattern P_GREEN = Pattern.compile("(\\d+) green");
+
+        public Set() {
+            blue = 0;
+            red = 0;
+            green = 0;
         }
-        public void reset(){
-            blue=0;
-            red=0;
-            green=0;
+
+        public void reset() {
+            blue = 0;
+            red = 0;
+            green = 0;
         }
-        public boolean getAmounts(String set){
-            Matcher matcherBlue = pBlue.matcher(set);
-            Matcher matcherRed = pRed.matcher(set);
-            Matcher matcherGreen = pGreen.matcher(set);
+
+        public boolean getAmounts(String set) {
+            Matcher matcherBlue = P_BLUE.matcher(set);
+            Matcher matcherRed = P_RED.matcher(set);
+            Matcher matcherGreen = P_GREEN.matcher(set);
             blue = matcherBlue.find() ? Integer.parseInt(matcherBlue.group(1)) : 0;
             red = matcherRed.find() ? Integer.parseInt(matcherRed.group(1)) : 0;
             green = matcherGreen.find() ? Integer.parseInt(matcherGreen.group(1)) : 0;
             return (blue <= 14 && green <= 13 && red <= 12);
         }
     }
-    private static int getId(String line){  //NOT necessary since the games go from 1 to 100
-        int indexOfEmpty = line.indexOf(' ')+1;
+
+    private static int getId(String line) {  //NOT necessary since the games go from 1 to 100
+        int indexOfEmpty = line.indexOf(' ') + 1;
         int indexOfColon = line.indexOf(':', indexOfEmpty);
         return Integer.parseInt(line.substring(indexOfEmpty, indexOfColon));
     }
+
     public static void main(String[] args) throws IOException {
         String absPath = Paths.get(System.getProperty("user.dir"), "src", "inputProblem2.txt").toString();
         BufferedReader reader = new BufferedReader(new FileReader(absPath));
         String line;
         int id = 0;
-        int idSum=0;
+        int idSum = 0;
         Set set = new Set();
 
-        while((line = reader.readLine()) != null){
+        while ((line = reader.readLine()) != null) {
             //int id = getId(line);
             id++;
             String[] sets = line.split(":")[1].split(";");
             int i = 0;
             boolean playable = true;
-            while(i < sets.length && playable){
+            while (i < sets.length && playable) {
                 playable = set.getAmounts(sets[i]);
                 i++;
                 set.reset();
             }
-            if (playable){
+            if (playable) {
                 idSum += id;
             }
         }
