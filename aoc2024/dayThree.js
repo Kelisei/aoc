ff8f8595c9ceddb78cb201cf10572b4ebc900bbe
kelisei <frankestudios71@gmail.com>
2024-12-04 18:49:13 +0000

    Better regex in 3

--- aoc2024/dayThree.js
+++ aoc2024/dayThree.js
@@ -1,9 +1,9 @@
 import fs from 'fs'
 const first = () => {
-    const regex = /mul\([0-9]+,[0-9]+\)/g;
+    const regex = /mul\((\d+)+,(\d+)+\)/g;
     const res = [...fs.readFileSync('inputs/input3.txt', 'utf-8').matchAll(regex)]
     .map(
-        (elem) => [...elem[0].matchAll(/[0-9]+/g)]
+        (elem) => [...elem[0].matchAll(/(\d+)+/g)]
         .map(match => Number(match[0]))
     ).reduce((acc, current) => acc + current[0] * current[1], 0);
     return res;
@@ -11,22 +11,23 @@ const first = () => {
 
 
 const second = () => {
-    const regex = /(do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\))/g;
-    let isEnabled = true;
-    let result = 0;
+    const regex = /(do\(\)|don't\(\)|mul\((\d+),(\d+)+\))/g;
+    const mulReg = /mul\((\d+),(\d+)\)/;
+    let enabled = true;
+    let res = 0;
 
-    readFileSync('inputs/input3.txt', 'utf-8').match(regex).forEach((instr) => {
+    fs.readFileSync('inputs/input3.txt', 'utf-8').match(regex).forEach((instr) => {
         if (instr === "do()") {
-            isEnabled = true;
+            enabled = true;
         } else if (instr === "don't()") {
-            isEnabled = false;
-        } else if (isEnabled && instr.startsWith('mul')) {
+            enabled = false;
+        } else if (enabled && instr.startsWith('mul')) {
             const [_, x, y] = instr.match(/mul\((\d+),(\d+)\)/);
-            result += parseInt(x) * parseInt(y);
+            res += parseInt(x) * parseInt(y);
         }
     });
 
-    return result;
+    return res;
 };
 
 console.log(second());
