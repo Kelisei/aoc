4028237cbb579d26e9b05baac63967e1bd023c01
Francisco <frankestudios71@gmail.com>
2025-12-03 20:58:26 -0300

    s

---
+++ 2025/day1/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	path := filepath.Join(pwd, "input.txt")
+	file, _ := os.ReadFile(path)
+	lines := strings.Split(string(file), "\n")
+	dial := 50
+	counter := 0
+	for i := range len(lines) {
+		line := lines[i]
+		if strings.HasPrefix(line, "L") {
+			strAmount := strings.Replace(line, "L", "", 1)
+			amount, _ := strconv.Atoi(strAmount)
+			fmt.Println(amount)
+			dial = (dial - amount) % 100
+			if dial == 0 {
+				counter++
+			}
+		} else if strings.HasPrefix(line, "R") {
+			strAmount := strings.Replace(line, "R", "", 1)
+			amount, _ := strconv.Atoi(strAmount)
+			fmt.Println(amount)
+			dial = (dial + amount) % 100
+			if dial == 0 {
+				counter++
+			}
+		}
+	}
+	fmt.Println("Password:", counter)
+}
