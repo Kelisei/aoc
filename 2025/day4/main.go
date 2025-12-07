f0b062abfd1e5dc17c91e93e0c0adc1038194118
Francisco Percara <frankestudios71@gmail.com>
2025-12-07 19:44:01 -0300

    Update main.go

--- 2025/day4/main.go
+++ 2025/day4/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"sync"
 )
@@ -21,76 +22,133 @@ func main() {
 	var wait sync.WaitGroup
 	height := len(matrix)
 	width := len(matrix[0])
-	counter := 0
-	for i := range height {
-		for j := range width {
-			wait.Add(1)
-			go func(i int, j int, height int, width int, matrix *[][]byte, counter *int, mtx *sync.Mutex, wait *sync.WaitGroup) {
-				defer wait.Done()
-				if (*matrix)[i][j] == '@' {
-					atsFound := 0
-					for x := max(0, j-1); x <= min(j+1, width-1); x++ {
-						for y := max(0, i-1); y <= min(i+1, height-1); y++ {
-							if x == j && y == i {
-								continue
-							}
-							if (*matrix)[y][x] == '@' {
-								atsFound++
-							}
-						}
-					}
-					if atsFound < 4 {
-						(*mtx).Lock()
-						(*counter)++
-						(*mtx).Unlock()
-					}
-				}
-			}(i, j, height, width, &matrix, &counter, &mtx, &wait)
-		}
-	}
-	wait.Wait()
-	fmt.Println(counter)
+	workers := runtime.NumCPU()
+	rowsPerWorker := height / workers
 
-	stack := make([][2]int, 0, 20)
-	counter = 0
-	hasChanged := true
-	for hasChanged {
-		hasChanged = false
-		for i := range height {
-			for j := range width {
-				wait.Add(1)
-				go func(i int, j int, height int, width int, matrix *[][]byte, counter *int, mtx *sync.Mutex, wait *sync.WaitGroup, stack *[][2]int, hasChanged *bool) {
-					defer wait.Done()
-					if (*matrix)[i][j] == '@' {
+	counter := 0
+
+	for w := range workers {
+		start := w * rowsPerWorker
+		end := start + rowsPerWorker
+
+		if w == workers-1 {
+			end = height
+		}
+
+		wait.Add(1)
+		go func(start, end int) {
+			defer wait.Done()
+
+			localCount := 0
+
+			for i := start; i < end; i++ {
+				for j := 0; j < width; j++ {
+					if matrix[i][j] == '@' {
 						atsFound := 0
 						for x := max(0, j-1); x <= min(j+1, width-1); x++ {
 							for y := max(0, i-1); y <= min(i+1, height-1); y++ {
 								if x == j && y == i {
 									continue
 								}
-								if (*matrix)[y][x] == '@' {
+								if matrix[y][x] == '@' {
 									atsFound++
 								}
 							}
 						}
 						if atsFound < 4 {
-							(*mtx).Lock()
-							*hasChanged = true
-							*stack = append(*stack, [2]int{i, j})
-							(*counter)++
-							(*mtx).Unlock()
+							localCount++
 						}
 					}
-				}(i, j, height, width, &matrix, &counter, &mtx, &wait, &stack, &hasChanged)
+				}
 			}
+			mtx.Lock()
+			counter += localCount
+			mtx.Unlock()
+
+		}(start, end)
+	}
+
+	wait.Wait()
+	fmt.Println(counter)
+
+	counter = 0
+	stack := make([][2]int, 0, 20)
+	hasChanged := true
+
+	for hasChanged {
+		hasChanged = false
+		stack = stack[:0]
+		workers := min(runtime.NumCPU(), height)
+		rowsPerWorker := (height + workers - 1) / workers
+
+		type result struct {
+			stack   [][2]int
+			count   int
+			changed bool
 		}
-		wait.Wait()
-		for len(stack) != 0 {
-			n := len(stack) - 1
-			val := stack[n]
-			stack = stack[:n]
-			matrix[val[0]][val[1]] = 'x'
+
+		results := make(chan result, workers)
+
+		for w := range workers {
+			start := w * rowsPerWorker
+			end := start + rowsPerWorker
+			if start >= height {
+				break
+			}
+			if end > height {
+				end = height
+			}
+
+			go func(start, end int) {
+				localStack := make([][2]int, 0, 32)
+				localCount := 0
+				localChanged := false
+
+				for i := start; i < end; i++ {
+					for j := 0; j < width; j++ {
+						if matrix[i][j] == '@' {
+							atsFound := 0
+							for x := max(0, j-1); x <= min(j+1, width-1); x++ {
+								for y := max(0, i-1); y <= min(i+1, height-1); y++ {
+									if x == j && y == i {
+										continue
+									}
+									if matrix[y][x] == '@' {
+										atsFound++
+									}
+								}
+							}
+							if atsFound < 4 {
+								localChanged = true
+								localCount++
+								localStack = append(localStack, [2]int{i, j})
+							}
+						}
+					}
+				}
+
+				results <- result{
+					stack:   localStack,
+					count:   localCount,
+					changed: localChanged,
+				}
+			}(start, end)
+		}
+
+		for range workers {
+			r := <-results
+			if r.changed {
+				hasChanged = true
+			}
+			counter += r.count
+			stack = append(stack, r.stack...)
+		}
+
+		for _, p := range stack {
+			i, j := p[0], p[1]
+			matrix[i][j] = 'x'
 		}
 	}
+
 	fmt.Println(counter)
 }
